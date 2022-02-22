be33aa5c53bd26fb9ff00202d117692ef101395c
Lee Przybylski <przybylskily@gmail.com>
2022-02-21 22:31:31 -0500

    Fixed problems with dplyr and tidyr

--- Predictions.R
+++ Predictions.R
@@ -9,3 +9,9 @@ names(regssn2021)
 df <- df %>% 
   mutate(Score = Final, Year = 2021) %>% 
   select(Team, Score, Year)
+
+List <- get_design(df)
+
+over_under_ols(List, "Baylor", "Oklahoma State")
+
+team_search(List, "Baylor")
