0812b2d930d0e98bf115aef793b470921a21f3d7
Lee Przybylski <przybylskily@gmail.com>
2022-03-12 22:54:12 -0500

    Added some predictions

--- Predictions.R
+++ Predictions.R
@@ -11,15 +11,24 @@ df <- df %>%
   select(Team, Score, Year) %>% 
   filter(!is.na(Score))
 
-List <- get_design(df)
+#df2 <- drop_incomplete_games(df)
+design <- get_design(df)
 
 List_nfl <- get_design(regssn2021)
 over_under_ols(List_nfl, "Chiefs", "Bills")
 over_under_ols(List_nfl, "Bengals", "Rams")
 
 
-over_under_ols(List, "UCSanDiego", "California")
-point_spread_ols(List, "Baylor", "OklahomaState")
+over_under_ols(design, "UCSanDiego", "California")
+point_spread_ols(design, "Baylor", "OklahomaState")
+winprob_emp(design, "Baylor", "OklahomaStat")
 
-team_detect(List, "Oklahoma")
+team_detect(design, "Iowa")
+team_detect(design, "Purdue")
 
+winprob_emp(design, "Purdue", "Iowa", home_effect = FALSE)
+winprob_ols(design, "Purdue", "Iowa", home_effect = FALSE)
+
+eML_ols(design, "Iowa", "Purdue", hBL = 105, aBL = -125, home_effect = FALSE)
+eSpread_ols(design, "Iowa", "Purdue", hspread = 1.5, aspread = -1.5, home_effect = FALSE)
+over_under_ols(design, "Iowa", "Purdue")
