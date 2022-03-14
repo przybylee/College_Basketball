0350d7754822eedc5f549a6fc97c1c24cedfa8be
Lee Przybylski <przybylskily@gmail.com>
2022-03-13 21:50:37 -0400

    Added predictions

--- Predictions.R
+++ Predictions.R
@@ -2,7 +2,7 @@ library(NFLpredictions)
 library(dplyr)
 library(tidyr)
 
-df <- read.csv("data/scores_2-21.csv")
+df <- read.csv("data/scores_3-13.csv")
 head(df)
 names(regssn2021)
 
@@ -11,7 +11,7 @@ df <- df %>%
   select(Team, Score, Year) %>% 
   filter(!is.na(Score))
 
-#df2 <- drop_incomplete_games(df)
+df2 <- drop_incomplete_games(df)
 design <- get_design(df)
 
 List_nfl <- get_design(regssn2021)
@@ -30,5 +30,34 @@ winprob_emp(design, "Purdue", "Iowa", home_effect = FALSE)
 winprob_ols(design, "Purdue", "Iowa", home_effect = FALSE)
 
 eML_ols(design, "Iowa", "Purdue", hBL = 105, aBL = -125, home_effect = FALSE)
-eSpread_ols(design, "Iowa", "Purdue", hspread = 1.5, aspread = -1.5, home_effect = FALSE)
+eSpread_ols(design, "Iowa", "Purdue", hspread = 2.5, aspread = -2.5, home_effect = FALSE)
 over_under_ols(design, "Iowa", "Purdue")
+
+
+#
+team_detect(design, "TexasSouther")
+winprob_emp(design, "TexasA&MCorpus", "TexasSouthern", home_effect = FALSE)
+eSpread_ols(design, "TexasA&MCorpus", "TexasSouthern", hspread = 3.5, home_effect = FALSE)
+eML_emp(design, "TexasA&MCorpus", "TexasSouthern", hBL = 145, aBL = -165, home_effect = FALSE)
+over_under_ols(design, "TexasA&MCorpus", "TexasSouthern")
+
+team_detect(design, "IndianaU")
+team_detect(design, "Wyoming")
+winprob_emp(design, "IndianaU", "Wyoming", home_effect = FALSE)
+eSpread_ols(design, "IndianaU", "Wyoming", hspread = -3.5, home_effect = FALSE)
+eML_emp(design, "IndianaU", "Wyoming", hBL = -170, aBL = 150, home_effect = FALSE)
+over_under_ols(design, "IndianaU", "Wyoming")
+
+team_detect(design, "NotreDame")
+team_detect(design, "Rutgers")
+winprob_emp(design, "NotreDame", "Rutgers", home_effect = FALSE)
+eSpread_ols(design, "NotreDame", "Rutgers", hspread = 1, home_effect = FALSE)
+eML_emp(design, "NotreDame", "Rutgers", hBL = -105, aBL = -115, home_effect = FALSE)
+over_under_ols(design, "NotreDame", "Rutgers")
+
+team_detect(design, "Michigan")
+team_detect(design, "Rutgers")
+winprob_emp(design, "NotreDame", "Rutgers", home_effect = FALSE)
+eSpread_ols(design, "NotreDame", "Rutgers", hspread = 1, home_effect = FALSE)
+eML_emp(design, "NotreDame", "Rutgers", hBL = -105, aBL = -115, home_effect = FALSE)
+over_under_ols(design, "NotreDame", "Rutgers")
